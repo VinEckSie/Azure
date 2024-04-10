ffd288e83cb4442ea6e8403c034faa2bb94c5644
VinEckSie <159125909+VinEckSie@users.noreply.github.com>
2024-04-10 20:16:31 +0000

    Update Peerberry function

---
+++ PIPBAPI/LoanResponse.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using PocketInvest.LoanModels;
+
+
+namespace PocketInvest.LoanResponseModels
+{
+    public class LoanResponse
+    {
+        public List<Loan> Data { get; set; }
+    }
+}
